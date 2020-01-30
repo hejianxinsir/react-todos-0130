9891a4e2f055e17ee7ab1f041cdb5e15d0324f9a
hejianxin <hejianxinsir@gmail.com>
2020-01-30 14:43:22 +0800

    基本骨架完成

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			newTodo: 'test',
+			todoList: [
+				{id: 1, title: '第一个待办'}
+			]
+		}
+	}
+
+	render(){
+		let todos = this.state.todoList.map((item, index)=>{
+			return <li>{item.title}</li>
+		})
+
+		return (
+			<div className="App">
+				<h1>我的待办</h1>
+				<div className="inputWrapper">
+					<input type="text" value={this.state.newTodo} />
+				</div>
+				<ol>		
+					{todos}
+				</ol>
+			</div>
+		)
+	}
+}
+
+export default App;
