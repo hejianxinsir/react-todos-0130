709d2adcd4e2a5a1465a42782ffcddb155f2a440
hejianxin <hejianxinsir@gmail.com>
2020-01-31 12:55:43 +0800

    添加leancloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,7 @@
+var AV = require('leancloud-storage');
+
+AV.init({
+   appId: "5NA1ULsUmgaPQbC5vlbwHPTQ-gzGzoHsz",
+   appKey: "RsdPdp4H4PPT8wHVo4HvR4On",
+   serverURLs: "https://xxx.example.com"
+});
