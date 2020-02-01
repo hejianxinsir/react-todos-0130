7b0a1bffaa20ed9b79c9bc5b3e002e23266ba137
hejianxin <hejianxinsir@gmail.com>
2020-02-01 14:23:46 +0800

    消除需要*onChange*的警告

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -80,8 +80,18 @@ export default class UserDialog extends Component {
 			<div className="UserDialog-Wrapper">
 				<div className="UserDialog">
 					<nav onChange={this.switch.bind(this)}>
-						<label><input type="radio" value="signUp" checked={this.state.selected === "signUp"}/> 注册</label>
-						<label><input type="radio" value="signIn" checked={this.state.selected === "signIn"}/> 登录</label>
+						<label>
+							<input type="radio" value="signUp"
+								checked={this.state.selected === "signUp"}
+								onChange={this.switch.bind(this)}
+							/> 注册
+						</label>
+						<label>
+							<input type="radio" value="signIn"
+								checked={this.state.selected === "signIn"}
+								onChange={this.switch.bind(this)}
+							/> 登录
+						</label>
 					</nav>
 					<div className="panes">
 						{this.state.selected === 'signUp' ? signUpForm : null}
