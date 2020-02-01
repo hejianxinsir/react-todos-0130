d61b86f8a3085e2068528cb4e0a2b8f7396d9678
hejianxin <hejianxinsir@gmail.com>
2020-02-01 14:03:44 +0800

    点击注册后使用 leancloud API 注册

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -3,5 +3,30 @@ var AV = require('leancloud-storage');
 AV.init({
    appId: "5NA1ULsUmgaPQbC5vlbwHPTQ-gzGzoHsz",
    appKey: "RsdPdp4H4PPT8wHVo4HvR4On",
-   serverURLs: "https://xxx.example.com"
+   serverURLs: "https://5na1ulsu.lc-cn-n1-shared.com"
 });
+
+export default AV
+
+export function signUp(username, password, successFn, errorFn){
+	var user = new AV.user()
+	user.setUsername(username)
+	user.setPassword(password)
+	user.signUp().then(function(loginedUser){
+		let user = getUserFromAVUser(loginedUser)
+		successFn.call(null, user)
+	}, function(error){
+		errorFn.call(null, user)
+	})
+
+	return undefined
+}
+
+function getUserFormAVUser(AVUser){
+	return {
+		id: AVUser.id,
+		...AVUser.attributes
+	}
+}
+
+
