ed55f364862bd583d00d265ec55d77062cffc073
hejianxin <hejianxinsir@gmail.com>
2020-02-02 20:36:34 +0800

    用户点击发送时，请求 leanCloud API

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -23,6 +23,14 @@ export function signUp(email, username, password, successFn, errorFn){
 	return undefined
 }
 
+export function sendPasswordResetEmail(email, successFn, errorFn){
+	AV.User.requestPasswordReset(email).then(function(success){
+		successFn.call()
+	},function(error){
+		console.dir(error)
+	})
+}
+
 export function signIn(username, password, successFn, errorFn){
 	AV.User.logIn(username, password).then(function(loginedUser){
 		let user = getUserFromAVUser(loginedUser)
